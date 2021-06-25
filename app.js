396bd3a9f85a262fb845ab684ab661a5ad0945b2
meehawk <meehawk@yahoo.com>
2021-06-25 14:39:18 +0530

    chore: initial commit

---
+++ app.js
@@ -0,0 +1,41 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('morgan');
+
+const indexRouter = require('./routes/index');
+const usersRouter = require('./routes/users');
+
+const app = express();
+
+// View engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+	next(createError(404));
+});
+
+// Error handler
+app.use((err, req, res) => {
+	// Set locals, only providing error in development
+	res.locals.message = err.message;
+	res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+	// Render the error page
+	res.status(err.status || 500);
+	res.render('error');
+});
+
+module.exports = app;
